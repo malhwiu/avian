f73e2ac7ef64a621ebcdad714e4660db4c548c94
Joona Aalto <jondolf.dev@gmail.com>
2026-06-20 19:20:51 +0300

    Update bevy_mod_debugdump

--- crates/avian2d/examples/debugdump_2d.rs
+++ crates/avian2d/examples/debugdump_2d.rs
@@ -1,7 +1,6 @@
 //! Run with:
 //! `cargo run --example debugdump_2d > dump.dot  && dot -Tsvg dump.dot > dump.svg`
 
-/*
 use avian2d::prelude::*;
 use bevy::prelude::*;
 
@@ -16,9 +15,4 @@ fn main() {
     // - FixedPostUpdate
     // - Update
     bevy_mod_debugdump::print_schedule_graph(&mut app, PhysicsSchedule);
-}
-*/
-
-fn main() {
-    todo!("not updated to Bevy 0.19 yet");
 }
